16b421ae346bc88b55f98e0bb4f718531fddd651
swamptea <96385599+swamptea@users.noreply.github.com>
2022-09-15 14:04:27 +0300

    Update MyConnectionTest.java

--- chatServer/src/test/java/ru/swamptea/javaChat/MyConnectionTest.java
+++ chatServer/src/test/java/ru/swamptea/javaChat/MyConnectionTest.java
@@ -1,8 +1,7 @@
 package ru.swamptea.javaChat;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.io.*;
